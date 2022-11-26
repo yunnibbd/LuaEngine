fde0e1dbb71d0cb2be30bf0b8fe0a197d8295efd
yunni <yunnibbd@163.com>
2022-11-26 21:28:32 +0800

    finish chapter 3

--- instruction.h
+++ instruction.h
@@ -1,6 +1,11 @@
 #ifndef __INSTRUCTION_H__
 #define __INSTRUCTION_H__
 #include <inttypes.h>
+#include "cbuffer.h"
+#include "opcodes.h"
+
+#define MAXARG_Bx (1 << 18 - 1)
+#define MAXARG_sBx (MAXARG_Bx >> 1)
 
 typedef uint32_t Instruction;
 
@@ -16,14 +21,55 @@ typedef struct{
 	int a, sbx;
 } TAsBx;
 
-int Opcode(Instruction i);
+inline int Opcode(Instruction i) {
+	return i & 0x3f;
+}
 
-TABC ABC(Instruction i);
+inline TABC ABC(Instruction i) {
+	TABC ret = {
+		i >> 6 & 0xff,
+		i >> 14 & 0x1ff,
+		i >> 23 & 0x1ff
+	};
+	return ret;
+}
 
-TABx ABx(Instruction i);
+inline TABx ABx(Instruction i) {
+	TABx ret = {
+		i >> 6 & 0xff,
+		i >> 14
+	};
+	return ret;
+}
 
-TAsBx AsBx(Instruction i);
+inline TAsBx AsBx(Instruction i) {
+	TABx temp = ABx(i);
+	TAsBx ret = {
+		temp.a,
+		temp.bx - MAXARG_sBx
+	};
+	return ret;
+}
 
-int Ax(Instruction i);
+inline int Ax(Instruction i) {
+	return i >> 6;
+}
+
+inline const char* OpName(Instruction i) {
+	return g_opcodes[Opcode(i)].name;
+}
+
+inline uint32_t OpMode(Instruction i) {
+	return g_opcodes[Opcode(i)].opMode;
+}
+
+inline uint32_t BMode(Instruction i) {
+	return g_opcodes[Opcode(i)].argBMode;
+}
+
+inline uint32_t CMode(Instruction i) {
+	return g_opcodes[Opcode(i)].argCMode;
+}
+
 
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,63 @@
 #include <stdio.h>
 #include "binary_chunk.h"
+#include "instruction.h"
 #include "cbuffer.h"
 #include "cvector.h"
 #include "utils.h"
 #include <string.h>
 #include <stdlib.h>
+
+OpCode g_opcodes[47] = {
+	/*     T  A     B       C    mode    name */
+	{0, 1, OpArgR, OpArgN, IABC,  "MOVE    "},
+	{0, 1, OpArgK, OpArgN, IABx,  "LOADK   "},
+	{0, 1, OpArgN, OpArgN, IABx,  "LOADKX  "},
+	{0, 1, OpArgU, OpArgU, IABC,  "LOADBOOL"},
+	{0, 1, OpArgU, OpArgN, IABC,  "LOADNIL "},
+	{0, 1, OpArgU, OpArgN, IABC,  "GETUPVAL"},
+	{0, 1, OpArgU, OpArgK, IABC,  "GETTABUP"},
+	{0, 1, OpArgR, OpArgK, IABC,  "GETTABLE"},
+	{0, 0, OpArgK, OpArgK, IABC,  "SETTABUP"},
+	{0, 0, OpArgU, OpArgN, IABC,  "SETUPVAL"},
+	{0, 0, OpArgK, OpArgK, IABC,  "SETTABLE"},
+	{0, 1, OpArgU, OpArgU, IABC,  "NEWTABLE"},
+	{0, 1, OpArgR, OpArgK, IABC,  "SELF    "},
+	{0, 1, OpArgK, OpArgK, IABC,  "ADD     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "SUB     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "MUL     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "MOD     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "POW     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "DIV     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "IDIV    "},
+	{0, 1, OpArgK, OpArgK, IABC,  "BAND    "},
+	{0, 1, OpArgK, OpArgK, IABC,  "BOR     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "BXOR    "},
+	{0, 1, OpArgK, OpArgK, IABC,  "SHL     "},
+	{0, 1, OpArgK, OpArgK, IABC,  "SHR     "},
+	{0, 1, OpArgR, OpArgN, IABC,  "UNM     "},
+	{0, 1, OpArgR, OpArgN, IABC,  "BNOT    "},
+	{0, 1, OpArgR, OpArgN, IABC,  "NOT     "},
+	{0, 1, OpArgR, OpArgN, IABC,  "LEN     "},
+	{0, 1, OpArgR, OpArgR, IABC,  "CONCAT  "},
+	{0, 0, OpArgR, OpArgN, IAsBx, "JMP     "},
+	{1, 0, OpArgK, OpArgK, IABC,  "EQ      "},
+	{1, 0, OpArgK, OpArgK, IABC,  "LT      "},
+	{1, 0, OpArgK, OpArgK, IABC,  "LE      "},
+	{1, 0, OpArgN, OpArgU, IABC,  "TEST    "},
+	{1, 1, OpArgR, OpArgU, IABC,  "TESTSET "},
+	{0, 1, OpArgU, OpArgU, IABC,  "CALL    "},
+	{0, 1, OpArgU, OpArgU, IABC,  "TAILCALL"},
+	{0, 0, OpArgU, OpArgN, IABC,  "RETURN  "},
+	{0, 1, OpArgR, OpArgN, IAsBx, "FORLOOP "},
+	{0, 1, OpArgR, OpArgN, IAsBx, "FORPREP "},
+	{0, 0, OpArgN, OpArgU, IABC,  "TFORCALL"},
+	{0, 1, OpArgR, OpArgN, IAsBx, "TFORLOOP"},
+	{0, 0, OpArgU, OpArgU, IABC,  "SETLIST "},
+	{0, 1, OpArgU, OpArgN, IABx,  "CLOSURE "},
+	{0, 1, OpArgU, OpArgN, IABC,  "VARARG  "},
+	{0, 0, OpArgU, OpArgU, IAx,   "EXTRAARG"},
+};
+
 
 void PrintHeader(Prototype* proto) {
 	const char* func_type = "main";
@@ -23,6 +76,53 @@ void PrintHeader(Prototype* proto) {
 	printf("%d locals, %d constants, %d functions\n", CVectorSize(proto->LocVars), CVectorSize(proto->Constants), CVectorSize(proto->Protos));
 }
 
+void PrintOperands(Instruction instruction) {
+	switch (OpMode(instruction)){
+		case IABC: {
+			TABC tabc = ABC(instruction);
+			printf("%d", tabc.a);
+			if (BMode(instruction) != OpArgN) {
+				if (tabc.b > 0xff) {
+					printf(" %d", -1 - tabc.b & 0xff);
+				}
+				else {
+					printf(" %d", tabc.b);
+				}
+			}
+			
+			if (CMode(instruction) != OpArgN) {
+				if (tabc.c > 0xff) {
+					printf(" %d", -1 - tabc.c & 0xff);
+				}
+				else {
+					printf(" %d", tabc.c);
+				}
+			}
+			break;
+		}
+		case IABx: {
+			TABx tabx = ABx(instruction);
+			printf("%d", tabx.a);
+			if (BMode(instruction) == OpArgK) {
+				printf(" %d", -1 - tabx.bx);
+			}
+			else if(BMode(instruction) == OpArgU) {
+				printf(" %d", tabx.bx);
+			}
+			break;
+		}
+		case IAsBx: {
+			TAsBx tasbx = AsBx(instruction);
+			printf("%d %d", tasbx.a, tasbx.sbx);
+			break;
+		}
+		case IAx: {
+			printf("%d", -1 - Ax(instruction));
+			break;
+		}
+	}
+}
+
 void PrintCode(Prototype* proto) {
 	CVector codes = proto->Code;
 	int len = CVectorSize(codes);
@@ -30,12 +130,15 @@ void PrintCode(Prototype* proto) {
 	for (int i = 0; i < len; ++i) {
 		 uint32_t* val = CVectorGet(codes, i);
 		 uint32_t* line = CVectorGet(proto->LineInfo, i);
+		 Instruction instruction = *val;
 		 if (flag) {
-			 printf("\t%d\t[%d]\t0x%08x\n", i + 1, *line, *val);
+			 printf("\t%d\t[%d]\t%s \t", i + 1, *line, OpName(instruction));
 		 }
 		 else {
-			 printf("\t%d\t[-]\t0x%08x\n", i + 1, *val);
+			 printf("\t%d\t[-]\t%s \t", i + 1, *line, OpName(instruction));
 		 }
+		 PrintOperands(instruction);
+		 printf("\n");
 	}
 }
 
@@ -100,7 +203,7 @@ void PrintDetail(Prototype* proto) {
 }
 
 int main(int argc, const char* const* argv) {
-	const char* file_name = "luac.out";
+	const char* file_name = "luac2.out";
 	if (argc > 1) {
 		file_name = argv[1];
 	}
